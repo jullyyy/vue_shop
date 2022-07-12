9b49369ef621beeb7343c3b3a284debe93b0d992
jullyyy <2253077237@qq.com>
2022-07-12 15:19:46 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,97 @@
 <template>
-  <div>
-    // 退出
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/head.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px':'200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区 -->
+        <!-- :router="true" 侧边栏开启路由模式 在通过惟一的path设置为index，作为路由的导航地址-->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          :unique-opened="true"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="true"
+          :default-active = "activePath"
+        >
+          <!-- 一级菜单 -->
+          <!-- 使用动态index，不然每次点击一个导航，其他导航也会跟着展开，
+          通过绑定各自的id，来绑定不同的index，使能够各自点击展开各自的导航
+          同时，index必须是字符串类型，而id是数值，所以要加一个空字符串转化一下 后面改成用path唯一标识了-->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
+            <!-- 一级菜单模板区 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import { Col } from 'element-ui'
 export default {
+  data() {
+    return {
+      // 左侧菜单数据
+      menulist: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      // 是否折叠
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath:''
+    }
+  },
+  // 生命周期函数
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     // 退出 基于token，只需销毁本地token，这样后续请求就没有能携带的token，必须重新登录生成新token才行
     logout() {
@@ -14,8 +99,73 @@ export default {
       // 编程式导航 重定向到login页面
       this.$router.push('/login')
     },
+    // 获取所有的菜单
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status != 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
+    },
+    // 点击按钮 切换菜单折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
+    },
+    // 保存链接的激活状态
+    saveNavState(activePath){
+      window.sessionStorage.setItem('activePath',activePath);
+      this.activePath = activePath;
+    }
   },
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  // el-header自带左右内边距，可以设定清除
+  padding-left: 0;
+  // 让内容上下居中，这里是为了让button不贴上下边
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    > span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+  .el-menu{
+    // 对齐优化
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+// 设置图标和字体之间的空格 其实也可以直接&nbsp;&nbsp;
+.iconfont{
+  margin-right: 10px;
+}
+.toggle-button{
+  background-color: #4a5064;
+  font-style: 10px;
+  text-align: center;
+  line-height: 24px;
+  color: #fff;
+  // 
+  letter-spacing: 0.2em;
+  // 鼠标变小手
+  cursor: pointer;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import './plugins/element.js'
+
 // 导入全局样式表
 import '../src/assets/css/global.css'
+
 // 导入字体图标
 import '../src/assets/fonts/iconfont.css'
+
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+    console.log(config)
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
